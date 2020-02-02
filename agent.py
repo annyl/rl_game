2c31f12ee9e0e1e68a1d1b1622e1b280b8522adb
trushkin-ilya <viciousde@gmail.com>
2020-02-02 19:06:50 +0300

    Make agents play with each other

--- agent.py
+++ agent.py
@@ -6,62 +6,69 @@ import torch.nn.functional as F
 
 
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
         super(DeepQNetwork, self).__init__()
         self.fc1 = nn.Linear(*input_dims, fc1_dims)
         self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-        self.fc3 = nn.Linear(fc2_dims, 1)
+        self.fc3 = nn.Linear(fc2_dims, n_actions)
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
 
-        def forward(self, observation):
-            state = torch.Tensor(observation).to(self.device)
-            x = F.relu(self.fc1(state))
-            x = F.relu(self.fc2(x))
-            actions = self.fc3(x)
-            return actions
+    def forward(self, observation):
+        state = torch.Tensor(observation).to(self.device)
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+        return actions
 
 
 class Agent:
     def __init__(self, gamma, epsilon, lr, input_dims,
-                 batch_size, n_actions, max_mem_size=1000000,
-                 eps_end=0.01, eps_dec=0.996):
-        self.n_actions = n_actions
+                 batch_size, action_space, max_mem_size=1000000,
+                 eps_min=0.01, eps_dec=0.996):
+        self.n_actions = len(action_space)
         self.epsilon = epsilon
         self.batch_size = batch_size
         self.gamma = gamma
-        self.action_space = [i for i in range(n_actions)]
-        self.net = DeepQNetwork(lr, input_dims, 256, 256)
+        self.action_space = {action: i for i,
+                             action in enumerate(action_space)}
+        self.net = DeepQNetwork(lr, input_dims, 256, 256, self.n_actions)
         self.mem_size = max_mem_size
+        self.mem_cntr = 0
         self.state_memory = np.zeros((max_mem_size, *input_dims))
         self.new_state_memory = np.zeros((max_mem_size, *input_dims))
         self.action_memory = np.zeros(
-            (max_mem_size, n_actions), dtype=np.uint8)
+            (max_mem_size, self.n_actions), dtype=np.uint8)
         self.reward_memory = np.zeros(max_mem_size)
         self.terminal_memory = np.zeros(max_mem_size, dtype=np.uint8)
+        self.eps_dec = eps_dec
+        self.eps_min = eps_min
 
     def store_transition(self, state, action, reward, new_state, terminal):
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state
         actions = np.zeros(self.n_actions)
-        actions[action] = 1.0
-        self.action_memory[index] = action
+        actions[self.action_space[action]] = 1.0
+        self.action_memory[index] = actions
         self.reward_memory[index] = reward
         self.terminal_memory[index] = 1 - terminal
         self.new_state_memory[index] = new_state
         self.mem_cntr += 1
 
-    def choose_action(self, observation):
+    def choose_action(self, observation, legal_actions):
+        if not legal_actions:
+            return 0
         rand = np.random.random()
         if rand < self.epsilon:
-            action = np.random.choice(self.action_space)
-        else:
-            actions = self.net.forward(observation)
-            action = torch.argmax(actions).item()
-        return action
+            return legal_actions[np.random.randint(len(legal_actions))]
+        actions = self.net.forward(observation)
+        legal_actions_idx = [self.action_space[legal_action]
+                             for legal_action in legal_actions]
+        action = torch.argmax(actions[legal_actions_idx]).item()
+        return list(self.action_space.keys())[legal_actions_idx[action]]
 
     def learn(self):
         if self.mem_cntr > self.batch_size:
@@ -70,18 +77,20 @@ class Agent:
         batch = np.random.choice(max_mem, self.batch_size)
         state_batch = self.state_memory[batch]
         action_batch = self.action_memory[batch]
+        action_values = [self.action_space[action]
+                         for action in self.action_space]
+        action_indices = np.dot(action_batch, action_values)
         reward_batch = self.reward_memory[batch]
         terminal_batch = self.terminal_memory[batch]
         new_state_batch = self.new_state_memory[batch]
 
         reward_batch = torch.Tensor(reward_batch).to(self.net.device)
         terminal_batch = torch.Tensor(terminal_batch).to(self.net.device)
-
         q = self.net.forward(state_batch).to(self.net.device)
         q_target = self.net.forward(state_batch).to(self.net.device)
         q_next = self.net.forward(new_state_batch).to(self.net.device)
         batch_index = np.arange(self.batch_size, dtype=np.int32)
-        q_target[batch_index, action_batch] = reward_batch + \
+        q_target[batch_index, action_indices] = reward_batch + \
             self.gamma * torch.max(q_next, dim=1)[0]*terminal_batch
         self.epsilon = max(self.epsilon * self.eps_dec, self.eps_min)
         loss = self.net.loss(q_target, q).to(self.net.device)
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from gym_checkers.checkers import Checkers
+from checkers import Checkers
 import agent
 import numpy as np
 
+
+def get_score(board: dict, turn: str):
+    #print(f'{len(board[turn]["men"])} {turn} men + {len(board[turn]["kings"])} {turn} kings = {len(board[turn]["men"]) + len(board[turn]["kings"]) * 2} score')
+    return len(board[turn]['men']) + len(board[turn]['kings']) * 2
+
+
 if __name__ == '__main__':
+    action_space = [(x, y) for x in range(32)
+                    for y in range(32) if x != y]
+    players = {'black': agent.Agent(gamma=0.99, epsilon=1.0, batch_size=64,
+                                    action_space=action_space, input_dims=[8*8+1], lr=0.003),
+               'white': agent.Agent(gamma=0.99, epsilon=1.0, batch_size=64,
+                                    action_space=action_space, input_dims=[8*8+1], lr=0.003)}
     env = Checkers()
-    brain = agent.Agent(gamma=0.99, epsilon=1.0, batch_size=64,
-                        n_actions=4, input_dims=[8], lr=0.003)
+    initial_state = env.save_state()
     scores = []
     eps_history = []
     n_games = 500
@@ -15,8 +26,37 @@ if __name__ == '__main__':
         print(f"episode={i}, score={score}")
         if i % 10 == 0 and i > 0:
             avg_score = np.mean(scores[max(0, i-10): i+1])
-            print(f"\taverage_score={avg_score:%3f}, eps={brain.epsilon:%3f}")
+            print(
+                f"\taverage_score={avg_score:.3f}, black_eps={players['black'].epsilon:.3f}, white_eps={players['white'].epsilon:.3f}")
         score = 0
-        eps_history.append(brain.epsilon)
-        print(f"board = {env.flat_board()}")
-        raise
+        env.restore_state(initial_state)
+        winner = None
+        moves = env.legal_moves()
+        board, turn, last_moved_piece = env.save_state()
+        brain = players[turn]
+        board_tensor = np.array(env.flat_board()).flatten()
+        encoded_turn = 1 if turn == 'black' else 0
+        observation = np.append(board_tensor, encoded_turn)
+        while not winner:
+            env.print_board()
+            action = brain.choose_action(observation, moves)
+            new_board, new_turn, _, moves, winner = env.move(*action)
+            turn_score = get_score(new_board, turn) - get_score(board, turn)
+            if(turn_score > 0):
+                raise
+            print(f'{turn} score = {turn_score}')
+            new_turn_score = get_score(
+                new_board, new_turn) - get_score(board, new_turn)
+            board = new_board
+            turn = new_turn
+            reward = turn_score - new_turn_score
+            score += reward
+            board_tensor = np.array(env.flat_board()).flatten()
+            encoded_turn = 1 if turn == 'black' else 0
+            new_observation = np.append(board_tensor, encoded_turn)
+            brain.store_transition(observation, action,
+                                   reward, new_observation, bool(winner))
+            brain.learn()
+            observation = new_observation
+            brain = players[turn]
+        scores.append(score)
